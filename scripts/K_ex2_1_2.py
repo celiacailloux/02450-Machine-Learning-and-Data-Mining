f07c7c3da3335ec174d4975afa53cb0d3efd872a
abc <abc@example.com>
2021-01-20 20:38:04 +0100

    changed plot of attributes to a subplots containing all possible combinations of attributes

--- scripts/K_ex2_1_2.py
+++ scripts/K_ex2_1_2.py
@@ -17,55 +17,76 @@ from K_ex2_1_1 import *
 # (requires data structures from ex. 2.1.1)
 
 from matplotlib.pyplot import figure, plot, title, legend, xlabel, ylabel, \
-    show, savefig, close
+    show, savefig, close, subplots
 from os.path import basename, splitext, join
 from itertools import combinations
+from math import ceil, floor
 
 
 # N length of y
 # M number of attribute names
 # C number of classes
 
+# %%
 # Data attributes to be plotted
 i = 0
 j = 1
 
 ##
 # Make a simple plot of the i'th attribute against the j'th attribute
-# Notice that X is of matrix type (but it will also work with a numpy array)
-# X = np.array(X) #Try to uncomment this line
-plot(X.iloc[:, i], X.iloc[:, j], 'o')
+# plot(X.iloc[:, i], X.iloc[:, j], 'o')
 
 # %%
 # Make another more fancy plot that includes legend, class labels, 
 # attribute names, and a title.
-f = figure()
+
+# Iterate over all possible 2D combinations of attributes and saveplot
+combinations_ij     = list(combinations(range(M),2))
+n_combinations_ij   = len(list(combinations_ij))
+
+# nrows, ncols    = 3, ceil(n_combinations_ij/3)
+nrows           = ceil(np.sqrt(n_combinations_ij))
+ncols           = floor(np.sqrt(n_combinations_ij))
+fig, axs        = subplots(nrows = nrows, ncols = ncols)#, sharex=True)
+fig.set_figwidth(ncols*3) #no need to determine height
+fig.set_figheight(nrows*3)
+# fig.subplots_adjust(wspace = 0, hspace=0.5)  
+
 title('NanoNose data')
+k = 0
+for combination in combinations_ij:
+    i = combination[0]
+    j = combination[1]
 
-# iterate through all classes and make a mask
-for c in range(C):
-    # select indices belonging to class c:
-    # (returns an df with Booleans)
-    class_mask = (y['class']==c) 
-    X_attribute_i = X.iloc[:, i][class_mask]
-    X_attribute_j = X.iloc[:, j][class_mask]
-    plot(X_attribute_i, X_attribute_j, 'o')
+    # iterate through all classes and make a mask
+    for c in range(C):
+        # select indices belonging to class c:
+        # (returns an df with Booleans)
+        class_mask      = (y['class']==c) 
+        X_attribute_i   = X.iloc[:, i][class_mask]
+        X_attribute_j   = X.iloc[:, j][class_mask]
+        # plot(X_attribute_i, X_attribute_j, 'o')
+        axs.flatten()[k].plot(X_attribute_i, X_attribute_j, 'o', label = classNames[c])
+        # axs.flatten()[k].set_aspect('equal')
+    
 
-legend(classNames)
-xlabel(attributeNames[i])
-ylabel(attributeNames[j])
+    axs.flatten()[k].set_xlabel(attributeNames[i])
+    axs.flatten()[k].set_ylabel(attributeNames[j])
+    k += 1
+       
+    
+axs[0,0].legend(bbox_to_anchor=(-1, 1), loc='upper left', borderaxespad=0.)
+fig.tight_layout()      
 
+show()
+# close('all')  
 # Save figure in the 'figures' directory
-exerciseName = splitext(basename(__file__))[0]
-saveFigTitle = exerciseName + '_' + 'attr' + '_' + attributeNames[i] + 'vs' + attributeNames[j]
-saveFigPath = join('../figures/',saveFigTitle)
-savefig(saveFigPath)
-
-# Output result to screen
-# show()
-close('all')
-
-# Iterate over all possible 2D combinations of attributeNames
-for combination in combinations(range(M),2):
-    print(combination)
+exerciseName    = splitext(basename(__file__))[0]
+# saveFigTitle = exerciseName + '_' + 'attr' + '_' + attributeNames[i] + 'vs' + attributeNames[j]
+saveFigTitle    = exerciseName + '_' + 'all_combinations_of_attr'
+saveFigPath     = join('../figures/',saveFigTitle)
+savefig(saveFigPath, dpi = 200)
+print('\'{}\ saved as figure'.format(saveFigTitle))    
 print('Ran Exercise 2.1.2')
+
+
