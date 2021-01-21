12993f29c567c48e75e5ff8e37ef6983c0ca71cd
abc <abc@example.com>
2021-01-21 05:45:13 +0100

    sklearn tools implemented rather than linear algebratools to compute PCA and variance explained ratio

--- scripts/K_ex2_1_3.py
+++ scripts/K_ex2_1_3.py
@@ -1,25 +1,78 @@
-# exercise 2.1.3
-# (requires data structures from ex. 2.2.1)
-from ex2_1_1 import *
+# ----------------------------------------------------------------------------
+# This script plot the variance explained ratio / variance explained by
+# principal components using sklearn modules
+# 
+# ML tags: Variance Explained, PCA, SVD, 
+#
+# 2021 celiacailloux@gmail.com
+#
+# inspiration from https://vitalflux.com/pca-explained-variance-concept-python-example/
+# 
+# exercise 2.1.3 (course 02450 DTU 2018)
+# ----------------------------------------------------------------------------
 
-from matplotlib.pyplot import figure, plot, title, xlabel, ylabel, show
-from scipy.linalg import svd
 
-# Subtract mean value from data
-Y = X - np.ones((N,1))*X.mean(axis=0)
+# (requires data structures from K_ex. 2.2.1)
+from K_ex2_1_1 import *
+from matplotlib.pyplot import figure, plot, title, xlabel, ylabel, show, \
+    savefig, step, legend, tight_layout
 
-# PCA by computing SVD of Y
-U,S,V = svd(Y,full_matrices=False)
+from sklearn.metrics import explained_variance_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
 
-# Compute variance explained by principal components
-rho = (S*S) / (S*S).sum() 
+save_figure = True
+
+# %%
+
+''' 
+when scaling the dataset, the explained variance varies from the course script
+where linear algebra is excecuted - you should follow up on this step. What 
+does the StandardScaler() do?
+'''
+
+# Scale the datasat (=subtract mean value from data)
+sc      = StandardScaler()
+sc.fit(X)
+X_std   = sc.transform(X)
+
+# %%
+
+# Determine transformed features (=PCA by computing SVD of Y)
+pca     = PCA()
+# X_pca   = pca.fit_transform(X_std)    # see note in previous section
+X_pca   = pca.fit_transform(X)
+
+# Determine explained variance explained_variance_ratio_ attri
+# (=Compute variance explained by principal components)
+exp_var_pca = pca.explained_variance_ratio_
+# exp_var_pca = pca.explained_variance_
+
+
+# Cummulative sum of eigenvalues; this will be used to create a step plot
+# for visualizing the variance explained by each PC
+cum_sum_eigenvalues = np.cumsum(exp_var_pca)
 
 # Plot variance explained
 figure()
-plot(range(1,len(rho)+1),rho,'o-')
+plot(range(0,len(exp_var_pca)), exp_var_pca, 'o-', label='Individual explained variance')
+plot(range(0,len(cum_sum_eigenvalues)), cum_sum_eigenvalues,'o-',label='Cumulative explained variance')
+
+legend(loc='best')
+
+
 title('Variance explained by principal components');
-xlabel('Principal component');
+xlabel('Principal component Index');
 ylabel('Variance explained');
+tight_layout()
 show()
+
+if save_figure:
+    # Save figure in the 'figures' directory
+    exerciseName    = splitext(basename(__file__))[0]
+    saveFigTitle    = exerciseName + '_' + 'variance_explained'
+    saveFigPath     = join('../figures/',saveFigTitle)
+    savefig(saveFigPath, dpi = 200)
+    print('\'{}\ saved as figure'.format(saveFigTitle))   
 
 print('Ran Exercise 2.1.3')
